d792b78af4f702c9b5a56f55d7939653cd805159
Ryunosuke Fukuda <fukuda_ryunosuke@lightcafe.co.jp>
2023-01-23 17:36:27 +0900

    コード修正

--- src/main/RomanNumberStringifierImpl.java
+++ src/main/RomanNumberStringifierImpl.java
@@ -5,7 +5,7 @@ public class RomanNumberStringifierImpl implements RomanNumberStringifier {
     @Override
     public String stringify(int n) {
 
-        if (n < 0 || n > 3999) return "The given number is out of the available range.";
+        if (n < 1 || n > 3999) return "The given number is out of the available range.";
 
         int[] numbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] romanNumbers = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
